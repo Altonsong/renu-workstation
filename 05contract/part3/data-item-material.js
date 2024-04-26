7ecb546d642623b3b32b5399ca26e6a4349c9f1d
Altonsong <129694621+Altonsong@users.noreply.github.com>
2024-04-26 13:28:26 -0400

    Version 202404261328 update Kai

--- 05contract/part3/data-item-material.js
+++ 05contract/part3/data-item-material.js
@@ -1,12 +1,52 @@
 const kitchenWallCabinet = [
 {
-  serial: "",
-  name: "",
-  image: "",
-  detail: "",
+  name: "High-Gloss Lacquer in White in Scandinavian Design",
+  image: "../../photos/material/wall-cabinet/high-gloss-lacquer-in-white-in-scandinavian-design.png",
 },
-
-
-
-
+{
+  name: "High-Gloss Lacquer in Grey in Scandinavian Design",
+  image: "../../photos/material/wall-cabinet/high-gloss-lacquer-in-grey-in-scandinavian-design.png",
+},
+{
+  name: "Lacquer in White Shaker in Transitional Design",
+  image: "../../photos/material/wall-cabinet/lacquer-in-white-shaker-in-transitional-design.png",
+},
+{
+  name: "Shaker White in Ascend Series",
+  image: "../../photos/material/wall-cabinet/lacquer-in-white-shaker-in-transitional-design.png",
+},
+{
+  name: "Shaker Grey in Ascend Series",
+  image: "../../photos/material/wall-cabinet/shaker-grey-in-ascend-series.png",
+},
+{
+  name: "Matte White in Ascend Series",
+  image: "../../photos/material/wall-cabinet/matte-white-in-ascend-series.png",
+},
+{
+  name: "Matte Grey in Ascend Series",
+  image: "../../photos/material/wall-cabinet/matte-grey-in-ascend-series.png",
+},
+{
+  name: "Wood Grey in Ascend Series",
+  image: "../../photos/material/wall-cabinet/wood-grey-in-ascend-series.png",
+},
+{
+  name: "White Oak in Ascend Series",
+  image: "../../photos/material/wall-cabinet/white-oak-in-ascend-series.png",
+},
+{
+  name: "High Gloss White in Ascend Series",
+  image: "../../photos/material/wall-cabinet/high-gloss-white-in-ascend-series.png",
+},
+{
+  name: "High Gloss Grey in Ascend Series",
+  image: "../../photos/material/wall-cabinet/high-gloss-grey-in-ascend-series.png",
+},
+{
+  name: "High Gloss Light Grey in Ascend Series",
+  image: "../../photos/material/wall-cabinet/high-gloss-light-grey-in-ascend-series.png",
+},
 ]
+
+
---
+++ data-final/addons/20240426-210Victoria-Kai/addons.js
@@ -0,0 +1,34 @@
+const addons = [
+  {
+    serial: "A1",
+    name: "Smooth Ceiling with Crown Moudling Removal",
+    image: "../../data-final/addons/20240426-210Victoria-Kai/a1.png",
+    detail: "Smooth Ceiling with Crown Moudling Removal.",
+    price: "4320",
+    isChecked: false,
+  },
+  {
+    serial: "A2",
+    name: "Remove Side Table and Fix Wall",
+    image: "",
+    detail: "Remove Drywall Side Table and Fix Wall",
+    price: "800",
+    isChecked: false,
+  },
+  {
+    serial: "A3",
+    name: "Fix Wall",
+    image: "",
+    detail: "Fix wall before painting.",
+    price: "800",
+    isChecked: false,
+  },
+  {
+    serial: "A4",
+    name: "Parking",
+    image: "",
+    detail: "Condo building does not provide visitor parking, for public parking fee.",
+    price: "500",
+    isChecked: false,
+  },
+]
