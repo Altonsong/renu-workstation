8272e759b4058d5940a03ee79f723650c261e098
Altonsong <129694621+Altonsong@users.noreply.github.com>
2024-05-31 14:48:49 -0400

    Version 202405311448 update 30-herons

--- 05contract/part3/data-item-material.js
+++ 05contract/part3/data-item-material.js
@@ -1,5 +1,74 @@
 
 
+const kitchenWallCabinet = [
+  {
+      code: "290-19",
+      name: "Grey Ash in Scandinavian Design",
+      image: "../../photos/material/base-cabinet/grey-ash-in-scandinavian-design.png",
+  },
+  {
+      code: "291-21",
+      name: "Mocha Ash in Scandinavian Design",
+      image: "../../photos/material/base-cabinet/mocha-ash-in-scandinavian-design.png",
+  },
+  {
+      code: "BWWF005",
+      name: "Latte Ash in Scandinavian Design",
+      image: "../../photos/material/base-cabinet/latte-ash-in-scandinavian-design.png",
+  },
+  {
+      code: "SHOPWB",
+      name: "Lacquer in White Shaker in Transitional Design",
+      image: "../../photos/material/base-cabinet/lacquer-in-white-shaker-in-transitional-design.png",
+  },
+  {
+      code: "SHALWB",
+      name: "Shaker White in Ascend Series",
+      image: "../../photos/material/wall-cabinet/shaker-white-in-ascend-series.png",
+  },
+  {
+      code: "SHALGB",
+      name: "Shaker Grey in Ascend Series",
+      image: "../../photos/material/wall-cabinet/shaker-grey-in-ascend-series.png",
+  },
+  {
+      code: "MTALWB",
+      name: "Matte White in Ascend Series",
+      image: "../../photos/material/wall-cabinet/matte-white-in-ascend-series.png",
+  },
+  {
+      code: "MTALGB",
+      name: "Matte Grey in Ascend Series",
+      image: "../../photos/material/wall-cabinet/matte-grey-in-ascend-series.png",
+  },
+  {
+      code: "WDALGB",
+      name: "Wood Grey in Ascend Series",
+      image: "../../photos/material/wall-cabinet/wood-grey-in-ascend-series.png",
+  },
+  {
+      code: "OKALWB",
+      name: "White Oak in Ascend Series",
+      image: "../../photos/material/wall-cabinet/white-oak-in-ascend-series.png",
+  },
+  {
+      code: "HGALWB",
+      name: "High Gloss White in Ascend Series",
+      image: "../../photos/material/wall-cabinet/high-gloss-white-in-ascend-series.png",
+  },
+  {
+      code: "HGALGB",
+      name: "High Gloss Grey in Ascend Series",
+      image: "../../photos/material/wall-cabinet/high-gloss-grey-in-ascend-series.png",
+  },
+  {
+      code: "HGALLGB",
+      name: "High Gloss Light Grey in Ascend Series",
+      image: "../../photos/material/wall-cabinet/high-gloss-light-grey-in-ascend-series.png",
+  },
+];
+
+
 const kitchenBaseCabinet = [
   {
       code: "290-19",
