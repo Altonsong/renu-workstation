63c9316064e4f2c8fa7b4851b5585190b9036915
Altonsong <129694621+Altonsong@users.noreply.github.com>
2024-07-24 09:39:52 -0400

    update kerry

---
+++ data-final/addons/20240724-88corporate-Kerry/addons.js
@@ -0,0 +1,43 @@
+const addons = [
+  {
+    serial: "A1",
+    name: "Full-height Backsplash",
+    image: "../../data-final/addons/20240724-88corporate-Kerry/a1.png",
+    detail: "Add a full height backsplash with the same countertop material in the kitchen area.",
+    price: "1870",
+    isChecked: false,
+  },
+  {
+    serial: "A2",
+    name: "Remodification of Kitchen Area",
+    image: "../../data-final/addons/20240724-88corporate-Kerry/a2.png",
+    detail: "Remodify kitchen range hood exhaust duct. Electrical rough-in work for both under-cabinet light fixture and track light. Install a bulkhead after existing ceiling light fixture is removed. Relocate the power outlets for the stove and range hood. Additional Lazy Susan system included.",
+    price: "4670",
+    isChecked: false,
+  },
+  {
+    serial: "A3",
+    name: "Kitchen Floor Tile",
+    image: "../../data-final/addons/20240724-88corporate-Kerry/a3.png",
+    detail: "Supply and install floor tile in the kitchen area.",
+    price: "2130",
+    isChecked: false,
+  },
+  {
+    serial: "A4",
+    name: "Trims in the kitchen entrance area",
+    image: "../../data-final/addons/20240724-88corporate-Kerry/a4.png",
+    detail: "Supply and install casing and jamb made of the same material as the baseboard at the entrance to the kitchen.",
+    price: "670",
+    isChecked: false,
+  },
+  {
+    serial: "A5",
+    name: "Light fixtures installations",
+    image: "",
+    detail: "Install a total of 3 light fixtures throughout the unit, fixtures will be provided by the client, the specific installation price depends on the specs of all the light fixtures.",
+    price: "TBD",
+    isChecked: false,
+  },
+  
+]
